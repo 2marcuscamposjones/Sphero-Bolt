9438fe608fc183c6a7220777aa64f1658332a4d3
Campos-Jones <2marcus.camposjones@gulllakecs.org>
2024-04-24 13:18:29 -0500

    Obstacle Course part 2

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -1,13 +1,13 @@
-ObstacleCourse.js
-//programmer: Marcus Campos-Jones
-//Team Member: Zach Kirk
-//Bolt ID:2FAE
-//Date. 4.16.2024
-//Program: Hello World - Sandbox
-//Program URL:https://edu.sphero.com/program/16700423/edit
-//player 1.Marcus campos-Jones
-//player 2 .Zack kirk
+// Programmer: Zach Kirk 
+// Team Member: Marcus Campos-Jones 
+// Bolt ID: 2FAE
+// Date 4.23.24: 
+// Program: Obstacle Course 
+// Program URL: https://edu.sphero.com/program/16743347/edit
 
+//Player 1. Zach Kirk
+//Player 2. Marcus Campos-Jones 
+/*
 //checkpoint one
 async function startProgram() {
 	setMainLed({ r: 200, g:55, b: 0 });
@@ -17,4 +17,13 @@ async function startProgram() {
 	await delay(1);
 	await roll((getHeading() + 270), 50, 2);
 }
+*/
 //player 1 code Marcus Campos-Jones
+
+/*
+// checkpoint two
+async function startProgram() {
+	await roll((getHeading() + 90), 40, 2);
+	await delay(1);
+	await roll((getHeading() + 90), 60, 2);
+	
