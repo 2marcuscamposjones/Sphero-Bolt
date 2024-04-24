5434c720202b3ddb866de7de5e006e6495cf202f
Campos-Jones <2marcus.camposjones@gulllakecs.org>
2024-04-24 13:02:24 -0500

    Obstacle Course part 1

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -5,3 +5,16 @@ ObstacleCourse.js
 //Date. 4.16.2024
 //Program: Hello World - Sandbox
 //Program URL:https://edu.sphero.com/program/16700423/edit
+//player 1.Marcus campos-Jones
+//player 2 .Zack kirk
+
+//checkpoint one
+async function startProgram() {
+	setMainLed({ r: 200, g:55, b: 0 });
+	await roll((getHeading() + 0), 60, 3);	
+	await delay(1);
+	await roll((getHeading() + 270), 40, 2);
+	await delay(1);
+	await roll((getHeading() + 270), 50, 2);
+}
+//player 1 code Marcus Campos-Jones
