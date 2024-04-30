eae6e39782d9dee85d318cf343fc6e70acb5d2bf
Campos-Jones <2marcus.camposjones@gulllakecs.org>
2024-04-30 12:28:17 -0500

    start code put into Maze Competition js

---
+++ MazeCompetition.js
@@ -0,0 +1,10 @@
+// Programmer: Marcus Campos-Jones 
+// Team Member: Zach Kirk 
+// Bolt ID: 2FAE
+// Date 4.30.24: 
+// Program: Maze Competition 
+// Program URL: https://edu.sphero.com/program/16788348/edit
+
+async function startProgram() {
+	// Write code here
+}
