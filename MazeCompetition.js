b27cc8c48bf6b73b967459fe57fd2ebb9816cd67
Campos-Jones <2marcus.camposjones@gulllakecs.org>
2024-04-30 13:20:08 -0500

    // Programmer: Zach- Start Text & audio Checkpoint Commit - Date:  4/30

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -6,5 +6,35 @@
 // Program URL: https://edu.sphero.com/program/16788348/edit
 
 async function startProgram() {
-	// Write code here
+	// Programmer: Marcus Campos - Start Text & audio checkpoint  Commit - Date: 
+
+
+
+	// programmer:        -  Blue LED checkpoint Commit - date:
+
+
+
+	// programmer:        -  Sound one checkpoint Commit - date:
+
+
+
+	// programmer:        -  Red LED checkpoint Commit - date:
+
+
+
+	// programmer:        -  Sound two checkpoint Commit - date:
+
+
+
+	// programmer:        -  green checkpoint Commit - date:
+
+
+
+	// programmer:        -  Purple Led & Sound three checkpoint Commit - date:
+
+
+
+	// programmer:        -  Finsh text & audio commit checkpoint Commit - date:
+
+
 }
