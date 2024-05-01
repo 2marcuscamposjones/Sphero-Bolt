6abf7931ceb90b46f85b1c916c300b00212b6a8c
Campos-Jones <2marcus.camposjones@gulllakecs.org>
2024-05-01 12:52:13 -0500

    programmer:   marcus campos-Red LED checkpoint Commit-date:5/1

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -6,23 +6,28 @@
 // Program URL: https://edu.sphero.com/program/16788348/edit
 
 async function startProgram() {
-	// Programmer: Marcus Campos - Start Text & audio checkpoint  Commit - Date: 
+	// Programmer: zack Kirk - Start Text & audio checkpoint  Commit - Date:4/30
 	await speak("start",true)
 	await scrollMatrixText('start!', { r: 66, g: 56, b: 255 }, 30, true)
 
-	// programmer:        -  Blue LED checkpoint Commit - date:
+
+	// programmer: Marcus Campos-Jones -  Blue LED checkpoint Commit - date:5/1
 	setMainLed({ r: 0, g:0, b: 255 });
 	await roll((getHeading() + 0), 60, 3);	
 	await delay(1);
 
-	// programmer:        -  Sound one checkpoint Commit - date:
+
+	// programmer:  Zack Kirk-Sound one checkpoint Commit-date:5/1
+	setMainLed({ r: 0, g:255, b: 0 });
+	await roll((getHeading() + 90), 60, 2);
+	await speak("Among Us",true)
 
 
-
-	// programmer:        -  Red LED checkpoint Commit - date:
-
-
-
+	// programmer:   marcus campos-Red LED checkpoint Commit-date:5/1
+	setMainLed({ r: 255, g:0, b: 0 });
+	await roll((getHeading() + 90), 50, 2);
+	await delay(2);
+	await roll((getHeading() + 45), 40, 2);
 	// programmer:        -  Sound two checkpoint Commit - date:
 
 
